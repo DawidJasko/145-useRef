a9a29b0129d0594a925c9da2bd297d4e6dc7bb99
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2022-11-23 17:45:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import "./style.css";
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      {`Samuraj Programowania kurs React od podstaw`}
     </div>
   );
 }
