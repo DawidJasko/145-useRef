07998689c550eeb21003658c1f4c51441dd7ab9b
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2022-11-23 19:03:00 +0100

    end commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from "react";
-import "./style.css";
+import React, { useRef, createRef, useState, useEffect } from 'react';
 
-export default function App() {
+import './style.css';
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const textInputRef = useRef();
+
+  useEffect(() => {
+    return () => (textInputRef.current = false);
+  }, []);
+  const numberInputRef = createRef();
+
+  const focusOnInput = () => textInputRef.current.focus();
+  const increaseCounter = () => setCounter(counter + 1);
+
+  useEffect(() => {
+    // textInputRef.current.focus();
+  }, []);
+
+  console.log(textInputRef.current);
+  console.log(numberInputRef.current);
+
   return (
     <div>
-      {`Samuraj Programowania kurs React od podstaw`}
+      <input ref={textInputRef} type="text" />
+      <input ref={numberInputRef} type="number" />
+      <button onClick={focusOnInput}>Ustaw focus na input</button>
+      <button onClick={increaseCounter}>Przerenderuj komponent</button>
     </div>
   );
-}
+};
+
+export default App;
